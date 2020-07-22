1cdd245f4404278f46e535b99f73e6f1b6735a1e
stevenhuang2020 <stevenhuang1017@gmail.com>
2020-07-22 14:08:22 +1200

    new

--- learn/imgFun.m
+++ learn/imgFun.m
@@ -1,3 +1,9 @@
-function im = imgFun(file)
-    im = imread(file)
+function [img,gray] = imgFun(file)
+    img = imread(file)
+    gray = grayImg(img)
+    imfinfo(file)
+end
+
+function gray_image = grayImg(img)
+    gray_image = rgb2gray(img);
 end
